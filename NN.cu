0d4e46c76f169e6cbb75d0a29758d643a61561ba
ahas11 <abdullahswelam51@gmail.com>
2024-08-09 17:15:59 -0400

    Added Comments

--- NN.cu
+++ NN.cu
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <cassert>
 
+//ReLU activation function
 __device__
 void activationFunction(int* n){
     if (*n < 0) {
@@ -12,6 +13,7 @@ void activationFunction(int* n){
     }
 }
 
+//Mean Squared Error function
 __device__
 void meanSquaredError(int* n, int* x) {
     *x = 0;
@@ -22,6 +24,7 @@ void meanSquaredError(int* n, int* x) {
     *x = *x / 2;
 }
 
+//Mean Squared Error Derivative to calculate backpropagation
 __device__
 void meanSquaredErrorDerivative(int* outputLayer, int* errTermDer) {
     int target = 25;
@@ -29,7 +32,7 @@ void meanSquaredErrorDerivative(int* outputLayer, int* errTermDer) {
 }
 
 
-
+//Forward propagation from Input Layer to Hidden Layer
 __global__
 void forwardPropItoH(int* vecInput, int* vecHidden, int* vecBias, 
                      float* vecWeightsx1, float* vecWeightsx2) {
@@ -42,6 +45,7 @@ void forwardPropItoH(int* vecInput, int* vecHidden, int* vecBias,
     }
 }
 
+//Forward propagation from Hidden Layer to Output Layer
 __global__
 void forwardPropHtoO(int* vecHidden, int* vecOutput, int* vecBias, 
                      float* vecWeightsy1, float* vecWeightsy2) {
@@ -64,6 +68,8 @@ void forwardPropHtoO(int* vecHidden, int* vecOutput, int* vecBias,
         }
     }
 }
+
+//Activation Function Derivative to calculate backpropagation
 __device__
 void activationFunctionDerivative(int* output, int* derTerm){
     if (*output < 0) {
@@ -72,6 +78,7 @@ void activationFunctionDerivative(int* output, int* derTerm){
     *derTerm = 1;
 }
 
+//Calculating the error for each node in the Hidden Layer
 __device__
 void backPropErrorH(float* weights1, float* weights2, int* errTerm, int* reluder_Hidden, int* outErrHidden) {
   int sumCounter = 0;
@@ -85,6 +92,7 @@ void backPropErrorH(float* weights1, float* weights2, int* errTerm, int* reluder
   }
 }
 
+//Calculating and adjusting the weights based on the weight change formula
 __device__
 void calculateNewWeight(float* weights1, float* weights2, int* vecOut, int* outputErr, float* lRate) {
     for(int i = 0; i < 3; i++) {
@@ -93,13 +101,19 @@ void calculateNewWeight(float* weights1, float* weights2, int* vecOut, int* outp
     } 
 }
 
+//Backpropagation from Output Layer to Hidden Layer
 __global__
 void backPropOtoH(int* vecHidden, int* vecOutput, int* vecBias, 
                   float* vecWeightsy1, float* vecWeightsy2, float* learning_Rate) {
     int index = threadIdx.x;
 
+    //Array to hold the errors of each output node
     __shared__ int err_term[2];
+
+    //Array to hold activation layer derivative values of the nodes in the Hidden Layer
     __shared__ int reluDerHidden[3];
+
+    //Array to hold total error values of the nodes in the Hidden Layer
     __shared__ int total_err_per_hidden[3];
 
     if(index<3){
@@ -115,14 +129,24 @@ void backPropOtoH(int* vecHidden, int* vecOutput, int* vecBias,
     if(index==2){
       backPropErrorH(vecWeightsy1, vecWeightsy2, err_term, reluDerHidden, total_err_per_hidden);
       calculateNewWeight(vecWeightsy1, vecWeightsy2, vecOutput, err_term,learning_Rate);
-      printf("%f\n%f\n%f\n", vecWeightsy1[0], vecWeightsy1[1], vecWeightsy1[2] );
     }
 }
 
 int main(void) {
+    //input layer values
     int x1 = 7;
     int x2 = 9;
 
+    //hidden layer initial values
+    int y1 = 0;
+    int y2 = 0;
+    int y3 = 0;
+
+    //output layer initial values
+    int o1 = 0;
+    int o2 = 0;
+
+    //weights connecting each node in the input layer to each node in the hidden layer
     float wx11 = 0.65;
     float wx12 = 0.55;
     float wx13 = 0.43;
@@ -130,41 +154,69 @@ int main(void) {
     float wx22 = 0.80;
     float wx23 = 0.80;
 
+    //weights connecting each node in the hidden layer to each node in the output layer
     float wy11 = 0.66;
     float wy12 = 0.45;
     float wy13 = 0.75;
-
     float wy21 = 0.30;
     float wy22 = 0.77;
     float wy23 = 0.63;
 
-    int y1 = 0;
-    int y2 = 0;
-    int y3 = 0;
-
-    int o1 = 0;
-    int o2 = 0;
-
+    //bias node values
     int b1 = 1;
+
+    //learning rate value for back propagation
     float learningrate = 0.1;
 
+    //input layer vector
     std::vector<int> input_layer({x1, x2});
+
+    //hidden layer vector
     std::vector<int> hidden_layer({y1, y2, y3});
+
+    //output layer vector
     std::vector<int> output_layer({o1, o2});
+
+    //bias vector
     std::vector<int> bias({b1});
+
+    //vector of  weights connecting first node node in the input layer to each node in the hidden layer
     std::vector<float> weightsx1({wx11, wx12, wx13});
+
+    //vector of  weights connecting second node node in the input layer to each node in the hidden layer
     std::vector<float> weightsx2({wx21, wx22, wx23});
+
+    //vector of  weights connecting each node in hidden layer to the first output node in the output layer
     std::vector<float> weightsy1({wy11, wy12, wy13});
+
+    //vector of weights connecting each node in hidden layer to the second output node 2 in the output layer
     std::vector<float> weightsy2({wy21, wy22, wy23});
 
+    //int pointer to input layer vector
     int* ptr_input;
+    
+    //int pointer to hidden layer vector
     int* ptr_hidden;
+    
+    //int pointer to output layer vector
     int* ptr_output;
+    
+    //int pointer to bias node
     int* ptr_bias;
+    
+    //float pointer to weightsx1 vector
     float* ptr_wx1;
+
+    //float pointer to weightsx2 vector
     float* ptr_wx2;
+
+    //float pointer to weightsy1 vector
     float* ptr_wy1;
+    
+    //float pointer to weightsy2 vector
     float* ptr_wy2;
+
+    //float pointer to learning rate
     float* ptr_learningRate;
 
 
@@ -194,21 +246,22 @@ int main(void) {
     cudaMemcpy(ptr_learningRate, &learningrate,sizeof(float), cudaMemcpyHostToDevice);
 
 
-    // Run the first kernel
+    // Running the forward propagation from input layer to hidden layer
     forwardPropItoH<<<1, 3>>>(ptr_input, ptr_hidden, ptr_bias, ptr_wx1, ptr_wx2);
 
-    // Wait for GPU to finish before accessing on host
+    // Waiting for GPU to finish before accessing on host
     cudaDeviceSynchronize();
 
-    // Run the second kernel
+    // Running the forward propagation from hidden layer to output layer
     forwardPropHtoO<<<1, 2>>>(ptr_hidden, ptr_output, ptr_bias, ptr_wy1, ptr_wy2);
 
-    // Wait for GPU to finish before accessing on host
+    // Waiting for GPU to finish before accessing on host
     cudaDeviceSynchronize();
 
+    // Running the backward propagation from output layer to hidden layer
     backPropOtoH<<<1, 3>>>(ptr_hidden, ptr_output, ptr_bias, ptr_wy1, ptr_wy2, ptr_learningRate);
 
-    // Wait for GPU to finish before accessing on host
+    // Waiting for GPU to finish before accessing on host
     cudaDeviceSynchronize();
 
     cudaFree(ptr_input);
